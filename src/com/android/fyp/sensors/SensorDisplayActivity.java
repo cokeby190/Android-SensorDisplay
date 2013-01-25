cc91891221687b0c42d68c56308843ceffc62aff
cokeby190 <cokeby190@gmail.com>
2013-01-25 14:49:57 +0800

    - Modified App to store files in folder where folder name = date/timestamp 	- i.e. 25_01_2013/25_01_2013 15-49/filename

--- src/com/android/fyp/sensors/SensorDisplayActivity.java
+++ src/com/android/fyp/sensors/SensorDisplayActivity.java
@@ -1,10 +1,12 @@
 package com.android.fyp.sensors;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -18,6 +20,12 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
+/**
+ * The Activity Displays Sensor Data to the User, including showing a dialog with additional 
+ * hardware information about the sensors when clicked. 
+ * @author Cheryl
+ *
+ */
 public class SensorDisplayActivity extends Activity implements OnClickListener {
 	
 	//Sensor Manager
@@ -30,26 +38,50 @@ public class SensorDisplayActivity extends Activity implements OnClickListener {
 	private TextView sensor_no;
 	private TextView tv_acc, tv_gyro, tv_magnet, tv_light, tv_prox, tv_temp;
 	private Button b_acc, b_gyro, b_magnet, b_light, b_prox, b_temp;
+	private Button b_display;
+	
+	//Dialog 
+	DialogAct show_dialog;
+	AlertDialog alert;
+	
+	//Save to External Mem
+	SaveExt save_ext;
+	String data_save = null;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        //Intent i = new Intent(this, AppPref_Act.class);
+        //startActivity(i);
+        
+        //initialise UI elements
         initialize();
+        
+        //save data to SD card
+        save_ext = new SaveExt(this);
+        save_ext.setState();
         
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         
         //list of all sensors
         deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
         
-        /**
-         * print out sensor types (int) in LOG
-         */
+        //print out sensor types (int) in LOG
         for (Sensor sensor : deviceSensors) {
             Log.v("Sensors", "" + sensor.getType());
         }
         
+        //Register Sensor Listener for all the sensors in the device. 
+        for (Sensor sensor : deviceSensors) {
+			
+			mSensorManager.registerListener(mSensorListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+		}
+		
+		sensor_no.setText("\n\nNumber of sensors detected: " + deviceSensors.size());
+
+        //Sensor Listener Object 
         mSensorListener = new SensorEventListener() {
 
 			@Override
@@ -57,6 +89,9 @@ public class SensorDisplayActivity extends Activity implements OnClickListener {
 				
 			}
 
+			/**
+			 * Function called whenever there are changes in sensor data
+			 */
 			@Override
 			public void onSensorChanged(SensorEvent event) {
 				
@@ -69,8 +104,12 @@ public class SensorDisplayActivity extends Activity implements OnClickListener {
 						y = event.values[1];
 						z = event.values[2];
 						
-						tv_acc.setText("\n\nACCELEROMETER: \n\nx-axis: " + x + " (m/s^2) \ny-axis: " + y + " (m/s^2) \nz-axis: " + z + " (m/s^2) \n");
+						tv_acc.setText("\nACCELEROMETER: \n\nx-axis: " + x + " (m/s^2) \ny-axis: " + y + " (m/s^2) \nz-axis: " + z + " (m/s^2) \n\n");
 						
+						data_save += time_stamp_convt() + "\t" + "Accelerometer" + "\t" + "x," + x + "\t" + "y," + y + "\t" + "z," + z + "\n";
+						save_ext.writeExt(data_save, "accelerometer");
+						
+						data_save = null;
 						break;
 					case Sensor.TYPE_GYROSCOPE:
 						
@@ -78,14 +117,18 @@ public class SensorDisplayActivity extends Activity implements OnClickListener {
 						y = event.values[1];
 						z = event.values[2];
 						
-						tv_gyro.setText("\n\nGYROSCOPE: \n\nx-axis: " + x + " (rad/s) \ny-axis: " + y + " (rad/s) \nz-axis: " + z + " (rad/s) \n");
+						tv_gyro.setText("\nGYROSCOPE: \n\nx-axis: " + x + " (rad/s) \ny-axis: " + y + " (rad/s) \nz-axis: " + z + " (rad/s) \n\n");
 						
+						data_save += time_stamp_convt() + "\t" + "Gyroscope" + "\t" + "x," + x + "\t" + "y," + y + "\t" + "z," + z + "\n";
+						save_ext.writeExt(data_save, "gyroscope");
+						
+						data_save = null;
 						break;
 					case Sensor.TYPE_LIGHT:
 						
 						float light = event.values[0];
 						
-						tv_light.setText("\n\nLIGHT: \n\n" + light + " (lux) \n");
+						tv_light.setText("\nLIGHT: \n\n" + light + " (lux) \n\n");
 						
 						break;
 					case Sensor.TYPE_MAGNETIC_FIELD:
@@ -94,21 +137,24 @@ public class SensorDisplayActivity extends Activity implements OnClickListener {
 						y = event.values[1];
 						z = event.values[2];
 						
-						tv_magnet.setText("\n\nMAGNETOMETER: \n\nx-axis: " + x + " (uT) \ny-axis: " + y + " (uT) \nz-axis: " + z + " (uT) \n");
+						tv_magnet.setText("\nMAGNETOMETER: \n\nx-axis: " + x + " (uT) \ny-axis: " + y + " (uT) \nz-axis: " + z + " (uT) \n\n");
+						data_save += time_stamp_convt() + "\t" + "Magnetometer" + "\t" + "x," + x + "\t" + "y," + y + "\t" + "z," + z + "\n";
+						save_ext.writeExt(data_save, "magnetometer");
 						
+						data_save = null;
 						break;
 					case Sensor.TYPE_PROXIMITY:
 						
 						float proximity = event.values[0];
 
-						tv_prox.setText("\n\nPROXIMITY: \n\n" + proximity + " (cm) \n\n");
+						tv_prox.setText("\nPROXIMITY: \n\n" + proximity + " (cm) \n\n");
 						
 						break;
-					case Sensor.TYPE_TEMPERATURE:
+					case Sensor.TYPE_AMBIENT_TEMPERATURE:
 						
 						float temp = event.values[0];
 
-						tv_temp.setText("\n\nTEMPERATURE: \n\n" + temp + " (deg Celsius) \n\n");
+						tv_temp.setText("\nTEMPERATURE: \n\n" + temp + " (deg Celsius) \n\n");
 						
 						break;
 				}
@@ -116,31 +162,36 @@ public class SensorDisplayActivity extends Activity implements OnClickListener {
 				//------------------------------------- UNSUPPORTED SENSORS -------------------------------------//
 				
 				if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) == null) {
-					tv_acc.setText("\n\nACCELEROMETER: \n\n" + "Not available on device" + "\n");
+					tv_acc.setText("\nACCELEROMETER: \n\n" + "Not available on device" + "\n\n");
 				}
 				if (mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) == null) {
-					tv_gyro.setText("\n\nGYROSCOPE: \n\n" + "Not available on device" + "\n");
+					tv_gyro.setText("\nGYROSCOPE: \n\n" + "Not available on device" + "\n\n");
 				}
 				if (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) == null) {
-					tv_magnet.setText("\n\nMAGNETOMETER: \n\n" + "Not available on device" + "\n");
+					tv_magnet.setText("\nMAGNETOMETER: \n\n" + "Not available on device" + "\n\n");
 				}
 				if (mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) == null) {
-					tv_light.setText("\n\nLIGHT: \n\n" + "Not available on device" + "\n");
+					tv_light.setText("\nLIGHT: \n\n" + "Not available on device" + "\n\n");
 				}
 				if (mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY) == null) {
-					tv_prox.setText("\n\nPROXIMITY: \n\n" + "Not available on device" + "\n");
+					tv_prox.setText("\nPROXIMITY: \n\n" + "Not available on device" + "\n\n");
 				}
-				if (mSensorManager.getDefaultSensor(Sensor.TYPE_TEMPERATURE) == null) {
-					tv_temp.setText("\n\nTEMPERATURE: \n\n" + "Not available on device" + "\n");
+				if (mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) == null) {
+					tv_temp.setText("\nTEMPERATURE: \n\n" + "Not available on device" + "\n\n");
 				}
 			
 			//------------------------------------- UNSUPPORTED SENSORS -------------------------------------//
 			
+				
+				
 			}
         	
         };
     }
-    
+
+	/**
+     * Initialise UI elements
+     */
     private void initialize() {
     	
     	sensor_no = (TextView) findViewById(R.id.sensor_no);
@@ -159,6 +210,9 @@ public class SensorDisplayActivity extends Activity implements OnClickListener {
     	b_prox = (Button) findViewById(R.id.proximity_button);
     	b_temp = (Button) findViewById(R.id.temp_button);
     	
+    	//b_display = (Button) findViewById(R.id.b_display);
+    	
+    	//b_display.setOnClickListener(this);
     	b_acc.setOnClickListener(this);
     	b_gyro.setOnClickListener(this);
     	b_magnet.setOnClickListener(this);
@@ -168,10 +222,14 @@ public class SensorDisplayActivity extends Activity implements OnClickListener {
     	
     }
     
+    /**
+     * Activity OnResume
+     */
     @Override
 	protected void onResume() { 
 		super.onResume();
 		
+		//register listener again onResume
 		for (Sensor sensor : deviceSensors) {
 			
 			mSensorManager.registerListener(mSensorListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
@@ -180,142 +238,101 @@ public class SensorDisplayActivity extends Activity implements OnClickListener {
 		sensor_no.setText("\n\nNumber of sensors detected: " + deviceSensors.size());
 	}
 
+    /**
+     * Activity OnPause
+     */
 	@Override
 	protected void onPause() {
 		super.onPause();
 		
+		//unregister listener again since User is not using them
 		for (Sensor sensor : deviceSensors) {
 			mSensorManager.unregisterListener(mSensorListener, sensor);
 		}
 	}
 
+	/**
+	 * OnClick Listener
+	 */
 	@Override
 	public void onClick(View v) {
 		
-		DialogAct show_dialog = new DialogAct();
-		AlertDialog alert;
+		show_dialog = new DialogAct();
 		
+		//Show the dialog when the user clicks the button
 		switch(v.getId()) {
+//			case R.id.b_display:
+//				//Intent display = new Intent(SensorDisplayActivity.this, DisplaySensor.class);
+//				Intent display = new Intent(SensorDisplayActivity.this, OpenDisplay.class);
+//				startActivity(display);
+//				break;
 			case R.id.accelerometer_button:
-				
-				if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
-
-					mAcc = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-					
-					Spanned accelerometer = Html.fromHtml("<big>Name: </big><br/>" + "<small>" + mAcc.getName() + "</small><br/><br/>"
-												+ "<big>Type: </big><br/>" + "<small>" + "Accelerometer" + "</small><br/><br/>"
-												+ "<big>Vendor: </big><br/>" + "<small>" + mAcc.getVendor() + "</small><br/><br/>"
-												+ "<big>Version: </big><br/>" + "<small>" + mAcc.getVersion() + "</small><br/><br/>"
-												+ "<big>Power: </big><br/>" + "<small>" + mAcc.getPower() + " mA</small><br/>");
-					
-					alert = show_dialog.dialog(this, "Accelerometer Information", accelerometer);
-				} else {
-					alert = show_dialog.dialog(this, "Accelerometer Information", Html.fromHtml("Sensor Unsupported by Device"));
-				}
-
-				alert.show();
-				
+				show_data(mAcc, Sensor.TYPE_ACCELEROMETER, "Accelerometer");
 				break;
 			case R.id.gyroscope_button:							
-				
-				if (mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null) {
-					
-					mGyro = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-					
-					Spanned gyroscope = Html.fromHtml("<big>Name: </big><br/>" + "<small>" + mGyro.getName()+ "</small><br/><br/>"
-												+ "<big>Type: </big><br/>" + "<small>" + "Gyroscope" + "</small><br/><br/>"
-												+ "<big>Vendor: </big><br/>" + "<small>" + mGyro.getVendor() + "</small><br/><br/>"
-												+ "<big>Version: </big><br/>" + "<small>" + mGyro.getVersion() + "</small><br/><br/>"
-												+ "<big>Power: </big><br/>" + "<small>" + mGyro.getPower() + " mA</small><br/>");
-					
-					alert = show_dialog.dialog(this, "Gyroscope Information", gyroscope);
-				} else {
-					alert = show_dialog.dialog(this, "Gyroscope Information", Html.fromHtml("Sensor Unsupported by Device"));
-				}	
-
-				alert.show();
-				
+				show_data(mGyro, Sensor.TYPE_GYROSCOPE, "Gyroscope");
 				break;
 			case R.id.light_button:
-				
-				if (mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null) {
-					
-					mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-					
-					Spanned light = Html.fromHtml("<big>Name: </big><br/>" + "<small>" + mLight.getName()+ "</small><br/><br/>"
-												+ "<big>Type: </big><br/>" + "<small>" + "Light" + "</small><br/><br/>"
-												+ "<big>Vendor: </big><br/>" + "<small>" + mLight.getVendor() + "</small><br/><br/>"
-												+ "<big>Version: </big><br/>" + "<small>" + mLight.getVersion() + "</small><br/><br/>"
-												+ "<big>Power: </big><br/>" + "<small>" + mLight.getPower() + " mA</small><br/>");
-					
-					alert = show_dialog.dialog(this, "Light Information", light);
-				} else {
-					alert = show_dialog.dialog(this, "Light Information", Html.fromHtml("Sensor Unsupported by Device"));
-				}
-				
-				alert.show();
-				
+				show_data(mLight, Sensor.TYPE_LIGHT, "Light");
 				break;
 			case R.id.magnetometer_button:
-				
-				if (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null) {
-					
-					mMagnet = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-					
-					Spanned magnetometer = Html.fromHtml("<big>Name: </big><br/>" + "<small>" + mMagnet.getName()+ "</small><br/><br/>"
-												+ "<big>Type: </big><br/>" + "<small>" + "Magnetometer" + "</small><br/><br/>"
-												+ "<big>Vendor: </big><br/>" + "<small>" + mMagnet.getVendor() + "</small><br/><br/>"
-												+ "<big>Version: </big><br/>" + "<small>" + mMagnet.getVersion() + "</small><br/><br/>"
-												+ "<big>Power: </big><br/>" + "<small>" + mMagnet.getPower() + " mA</small><br/>");
-					
-					alert = show_dialog.dialog(this, "Magnetometer Information", magnetometer);
-				} else {
-					alert = show_dialog.dialog(this, "Proximity Information", Html.fromHtml("Sensor Unsupported by Device"));
-				}
-				
-				alert.show();
+				show_data(mMagnet, Sensor.TYPE_MAGNETIC_FIELD, "Magnetometer");
 				break;
 			case R.id.proximity_button:
-				
-				if (mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY) != null) {
-					
-					mProx = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-					
-					Spanned proximity = Html.fromHtml("<big>Name: </big><br/>" + "<small>" + mProx.getName()+ "</small><br/><br/>"
-												+ "<big>Type: </big><br/>" + "<small>" + "Proximity" + "</small><br/><br/>"
-												+ "<big>Vendor: </big><br/>" + "<small>" + mProx.getVendor() + "</small><br/><br/>"
-												+ "<big>Version: </big><br/>" + "<small>" + mProx.getVersion() + "</small><br/><br/>"
-												+ "<big>Power: </big><br/>" + "<small>" + mProx.getPower() + " mA</small><br/>");
-					
-					alert = show_dialog.dialog(this, "Proximity Information", proximity);
-				} else {
-					alert = show_dialog.dialog(this, "Proximity Information", Html.fromHtml("Sensor Unsupported by Device"));
-				}
-				
-				alert.show();
+				show_data(mProx, Sensor.TYPE_PROXIMITY, "Proximity");
 				break;
 			case R.id.temp_button:
-				
-				if (mSensorManager.getDefaultSensor(Sensor.TYPE_TEMPERATURE) != null) {
-					
-					mTemp = mSensorManager.getDefaultSensor(Sensor.TYPE_TEMPERATURE);
-					
-					Spanned temp = Html.fromHtml("<big>Name: </big><br/>" + "<small>" + mTemp.getName()+ "</small><br/><br/>"
-												+ "<big>Type: </big><br/>" + "<small>" + "Temperature" + "</small><br/><br/>"
-												+ "<big>Vendor: </big><br/>" + "<small>" + mTemp.getVendor() + "</small><br/><br/>"
-												+ "<big>Version: </big><br/>" + "<small>" + mTemp.getVersion() + "</small><br/><br/>"
-												+ "<big>Power: </big><br/>" + "<small>" + mTemp.getPower() + " mA</small><br/>");
-					
-					alert = show_dialog.dialog(this, "Temperature Information", temp);
-				} else {
-					alert = show_dialog.dialog(this, "Temperature Information", Html.fromHtml("Sensor Unsupported by Device"));
-				}
-				
-				alert.show();
-				
+				show_data(mTemp, Sensor.TYPE_AMBIENT_TEMPERATURE, "Temperature");
 				break;
 		
 		}
 	}
 	
+	/**
+	 * Function to display sensor information on Android UI in the form of a dialog
+	 * @param sensor		Sensor datatype to initialise particular sensor
+	 * @param sensor_type	type of sensor, TYPE.Sensor_xx
+	 * @param type_str		String to print out TYPE of sensor
+	 */
+	private void show_data(Sensor sensor, int sensor_type, String type_str) {
+		if (mSensorManager.getDefaultSensor(sensor_type) != null) {
+			
+			sensor = mSensorManager.getDefaultSensor(sensor_type);
+			
+			Spanned sensor_data = Html.fromHtml("<big>Name: </big><br/>" + "<small>" + sensor.getName()+ "</small><br/><br/>"
+										+ "<big>Type: </big><br/>" + "<small>" + type_str + "</small><br/><br/>"
+										+ "<big>Vendor: </big><br/>" + "<small>" + sensor.getVendor() + "</small><br/><br/>"
+										+ "<big>Version: </big><br/>" + "<small>" + sensor.getVersion() + "</small><br/><br/>"
+										+ "<big>Power: </big><br/>" + "<small>" + sensor.getPower() + " mA</small><br/>");
+			
+			alert = show_dialog.dialog(this, type_str + " Information", sensor_data);
+		} else {
+			alert = show_dialog.dialog(this, type_str + " Information", Html.fromHtml("Sensor Unsupported by Device"));
+		}
+		
+		alert.show();
+	}	
+	
+	
+	/**
+	 * Convert time in Millis to dateformat specified by SimpleDateFormat
+	 * @return	String of converted timestamp from Millis
+	 */
+	protected String time_stamp_convt() {
+		// Create a DateFormatter object for displaying date information.
+        SimpleDateFormat formatter = new SimpleDateFormat("ddMMyy hh:mm:ss.SSS");
+
+        // Get date and time information in milliseconds
+        long now = System.currentTimeMillis();
+
+        // Create a calendar object that will convert the date and time value
+        // in milliseconds to date. We use the setTimeInMillis() method of the
+        // Calendar object.
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(now);
+         
+        System.out.println(formatter.format(calendar.getTime()));
+        
+        return formatter.format(calendar.getTime());
+	}
 }
